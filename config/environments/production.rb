873e37b9b0424338a8f2918f4dd1e772fa59d531
Adushar <vzaeab89@mail.ru>
2018-07-15 20:38:37 +0300

    Remove

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,11 +38,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
-  config.action_mailer.default_url_options = { host: 'http://188.166.60.112' }
-  config.default_url_options = { host: "http://188.166.60.112" }
-
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
