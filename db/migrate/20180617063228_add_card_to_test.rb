383d5afa8064ba509bf189c30d8b672239fe67e7
Adushar <vzaeab89@mail.ru>
2018-07-15 20:57:22 +0300

    m

--- db/migrate/20180617063228_add_card_to_test.rb
+++ db/migrate/20180617063228_add_card_to_test.rb
@@ -1,6 +1,6 @@
 class AddCardToTest < ActiveRecord::Migration[5.1]
   def change
-    add_column :tests, :card, :has_many
+    add_column :tests, :cards, :has_many
     add_column :tests, :test_results, :has_many
   end
 end
