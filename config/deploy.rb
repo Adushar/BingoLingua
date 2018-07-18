45ce3f4fdb586113d424c50185f80fbf0dc9e2bd
Adushar <vzaeab89@mail.ru>
2018-07-18 16:29:22 +0300

    restart unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -90,7 +90,7 @@ task :deploy do
   end
 
   # you can use `run :local` to run tasks on local machine before of after the deploy scripts
-  # run(:local){ say 'done' }
+  run(:local){ command "mina unicorn:restart" }
 end
 
 # For help in making your deploy script, see the Mina documentation:
