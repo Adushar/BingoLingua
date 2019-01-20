7acccc686cebc8b606ba8a91801b35c41d2c65e2
Adushar <vzaeab89@mail.ru>
2019-01-20 11:47:31 +0200

    Complete top 10 page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,5 @@
 class TopController < ApplicationController
   def index
-    @top = TestResult.where('created_at >= ?', 1.month.ago).order(:last_result).group_by(&:user_id)
-    @top
+    @top_users = User.all.sort_by { |e| e.points.sum(:points) }.last(10).reverse
   end
 end
