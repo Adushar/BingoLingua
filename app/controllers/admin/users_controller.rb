859d18cba9775a974cf811350a5bda59f27cb017
Adushar <vzaeab89@mail.ru>
2019-04-21 14:02:48 +0300

    Create user without email cofirmation from admin panel

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,21 @@
 module Admin
   class UsersController < Admin::ApplicationController
+    def create
+      resource = resource_class.new(resource_params)
+      authorize_resource(resource)
+      resource.skip_confirmation!
+
+      if resource.save
+        redirect_to(
+          [namespace, resource],
+          notice: translate_with_resource("create.success"),
+        )
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, resource),
+        }
+      end
+    end
     # To customize the behavior of this controller,
     # you can overwrite any of the RESTful actions. For example:
     #
