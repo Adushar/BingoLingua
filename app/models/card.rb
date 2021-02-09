30e9e9e855271e4783532bf9939b0e3908d5a990
Adushar <Adushar@mail.ru>
2021-02-09 11:04:08 +0200

    Update card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,8 +1,8 @@
 class Card < ApplicationRecord
-  has_many :selected_cards
+  has_many :selected_cards, dependent: :destroy
   has_many :users, :through => :selected_cards
-  has_many :learned_words
-  has_many :shown_cards
+  has_many :learned_words, dependent: :nullify
+  has_many :shown_cards, dependent: :nullify
   has_one :language
   belongs_to :test, optional: true, foreign_key: 'test_id'
   # validates_presence_of :description
