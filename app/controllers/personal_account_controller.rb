95d6bbe4e5ad30a14e409b94cf9ba6fc2be59078
Adushar <Adushar@mail.ru>
2021-03-14 12:53:50 +0200

    Fix personal_account

--- app/controllers/personal_account_controller.rb
+++ app/controllers/personal_account_controller.rb
@@ -4,7 +4,7 @@ class PersonalAccountController < ApplicationController
     @subscribe_days_left = current_user.subscribe_left
     @score = current_user.scores
     @monthly_score = current_user.monthly_score
-    @learned_words = LearnedWord.where(user: current_user)
+    @learned_words = LearnedWord.where(user: current_user).where.not(card: nil)
     @result = {
       cards: @learned_words.count,
       tests: current_user.test_results.distinct.pluck(:test_id).count,
