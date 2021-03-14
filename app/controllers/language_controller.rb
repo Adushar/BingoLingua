a87e3fd06ccab6d7e1ca721514ba6df8d56e41ad
Adushar <Adushar@mail.ru>
2021-03-14 14:52:22 +0200

    Fix language switching issue

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -1,10 +1,11 @@
 class LanguageController < ApplicationController
   before_action :authenticate_user!
   def update
+    user = current_user
     language = Language.find(params[:id])
     if language
-      language.users << current_user
-      if language.save
+      user.language = language
+      if user.save
         flash[:notice] = "Your language now is set as #{language.name.capitalize}"
         redirect_to root_path
       end
